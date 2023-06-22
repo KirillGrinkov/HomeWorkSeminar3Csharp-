dbb94d10ffbd7b51d788984ac7336b1d0642a046
Kirill Grinkov <79636456163@yandex.ru>
2023-06-22 21:09:49 +0400

    addtask23

---
+++ task23/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+
+Console.Clear();
+
+
+Console.Write("Введите число: ");
+string userImput = Console.ReadLine() ??"";
+int num = int.Parse(userImput);
+int index = 1;
+int degree = 3;
+
+while(index <= num)
+{
+    Console.WriteLine( Math.Pow(index, degree));
+    index++;
+    
+}
