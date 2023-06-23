a2bf710b2706de1bbba5ffdf8f6541e6e6e8d127
KirillGrinkov <135019765+KirillGrinkov@users.noreply.github.com>
2023-06-23 14:10:04 +0400

    Update task19.cs

--- task19/task19.cs
+++ task19/task19.cs
@@ -9,7 +9,7 @@ int number = int.Parse(userImput);
 
 if ((number % 10 == number / 10000) && ((number / 1000) % 10 == (number / 10) % 10))
     {
-        Console.WriteLine($"{number}  полиндром");  
+        Console.WriteLine($"{number}  палиндром");  
     }
  else
- Console.WriteLine($"{number} не полиндром");
+ Console.WriteLine($"{number} не палиндром");
